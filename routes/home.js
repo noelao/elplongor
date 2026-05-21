d0cb306f540c1c9fd5541ca858ccd36c3214d00d
unknown <bangunjamduamalam@gmail.com>
2026-05-21 10:00:54 +0700

    update

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,9 @@ const home = express.Router();
 home.get('/', (req, res) => {
     res.render('index');
 });
+home.get('/donate', (req, res) => {
+    res.render('donate');
+});
 
 
 module.exports = home;
